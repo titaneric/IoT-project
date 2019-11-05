d3d479468cbec66d8ba554f6e2e91b1bda92ddbb
titaneric <chenyihuang001@gmail.com>
2019-11-06 00:11:10 +0800

    :penguin: Test groupby window in text

--- text.py
+++ text.py
@@ -2,9 +2,14 @@ from datetime import datetime
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from pyspark.sql.functions import window
 
 datetime_format = r"%Y/%m/%d %H:%M:%S"
-datetime_convert = lambda datetime_str: datetime.strptime(datetime_str, datetime_format)
+
+
+def datetime_convert(datetime_str): return datetime.strptime(
+    datetime_str, datetime_format)
+
 
 def define_schema():
     header = (
@@ -14,28 +19,31 @@ def define_schema():
     )
     header = list(map(lambda x: x.replace(' ', '_'), header.split(',')))
     types = [TimestampType, StringType, StringType, StringType, StringType, StringType,
-            StringType, StringType, StringType, IntegerType, IntegerType,
-            StringType, IntegerType, IntegerType, TimestampType]
-    fields = [StructField(field_name, dataframe_type()) for field_name, dataframe_type in zip(header, types)]
+             StringType, StringType, StringType, IntegerType, IntegerType,
+             StringType, IntegerType, IntegerType, TimestampType]
+    fields = [StructField(field_name, dataframe_type())
+              for field_name, dataframe_type in zip(header, types)]
     schema = StructType(fields)
     return schema
+
 
 def convert_type(line):
     elements = line.split(",")
     types = [datetime_convert, str, str, str, str, str,
-            str, str, str, int, int,
-            str, int, int, datetime_convert]
+             str, str, str, int, int,
+             str, int, int, datetime_convert]
     for i, (element, type_func) in enumerate(zip(elements, types)):
         elements[i] = type_func(element)
     return elements
+
 
 if __name__ == "__main__":
     schema = define_schema()
 
     spark = SparkSession\
-            .builder\
-            .appName("Test basic logs")\
-            .getOrCreate()
+        .builder\
+        .appName("Test basic logs")\
+        .getOrCreate()
 
     sc = spark.sparkContext
     # Load a text file and convert each line to a Row.
@@ -47,7 +55,19 @@ if __name__ == "__main__":
     # Creates a temporary view using the DataFrame
     schema_logs.createOrReplaceTempView("logs")
 
-    # SQL can be run over DataFrames that have been registered as a table.
-    results = spark.sql("SELECT byte_receive FROM logs")
+    group_by_one_minute = schema_logs.groupBy(window("start_time", "1 minute"))
+    # Occurances
+    grouped_occurances = group_by_one_minute.count().orderBy('window')
+    grouped_occurances.show(20, False)
 
-    results.show()
+    # Byte receive
+    grouped_byte_receive = group_by_one_minute\
+        .sum("byte_receive").orderBy('window')
+    grouped_byte_receive.show(20, False)
+
+    # Byte send
+    grouped_send = group_by_one_minute\
+        .sum("byte_send").orderBy('window')
+    grouped_send.show(20, False)
+
+    schema_logs.printSchema()
