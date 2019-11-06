044824abda8250bf7ed2f35554f61e4a2edadf6f
titaneric <chenyihuang001@gmail.com>
2019-11-06 17:56:33 +0800

    :penguin: Test groupby on structured streaming

--- gen_log.py
+++ gen_log.py
@@ -11,12 +11,21 @@ line = ("{receive_time},{type_},{source_ip},{from_port},{dest_ip},{to_port},"
         "{application},{action},{session_end},{byte_receive},{byte_send},"
         "{ip_protocol},{packet_receive},{packet_send},{start_time}")
 
+ip_choices = ["140.113.216.230", "140.113.24.185"]
+source_choices = [True, False]
+
 def generate_log_line():
     fake = Faker()
     start_time = datetime.now()
     receive_time = start_time + timedelta(seconds=3)
-    source_ip = fake.ipv4()
-    dest_ip = fake.ipv4()
+    start_time = start_time.strftime(r"%Y/%m/%d %H:%M:%S")
+    receive_time = receive_time.strftime(r"%Y/%m/%d %H:%M:%S")
+    ip = random.choice(ip_choices)
+    source = random.choice(source_choices)
+
+    source_ip = ip if source else fake.ipv4()
+    dest_ip = fake.ipv4() if source else ip
+
     to_port = 80
     from_port = 22
     type_="end"
@@ -36,6 +45,6 @@ def generate_log_line():
 
 
 if __name__ == "__main__":
-    for i in range(3):
+    for i in range(10):
         log = generate_log_line()
         print(log)
--- spark_process.py
+++ spark_process.py
@@ -1,6 +1,8 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+from gen_log import ip_choices
+
 header = (
     "receive time,type,source ip,from port,dest ip,to port,"
     "application,action,session end,byte receive,byte send,"
@@ -8,7 +10,7 @@ header = (
 )
 
 header = list(map(lambda x: x.replace(' ', '_'), header.split(',')))
-
+window_len = 10
 sc = SparkContext("local[2]", "LogAggregator")
 ssc = StreamingContext(sc, 1)
 ssc.checkpoint("checkpoint")
@@ -16,18 +18,24 @@ ssc.checkpoint("checkpoint")
 def split_log(log):
     log = log.split(",")
     log_dict = dict(zip(header, log))
-    return log_dict["source_ip"], int(log_dict["byte_receive"])
+    ip = log_dict["source_ip"] if log_dict["source_ip"] in ip_choices else log_dict["dest_ip"]
+    # return ip, int(log_dict["byte_receive"])
+    return ip, 1
 
 def update_val(val, previous):
     if previous is None:
         previous = 0
-    return sum(val, previous)
+    return previous + val
+
+def remove_val(current_sum, previous):
+    return current_sum - previous
 
 lines = ssc.socketTextStream("localhost", 6666)
 state = lines.map(split_log)
-running_cnts = state.updateStateByKey(update_val)
-
-running_cnts.pprint()
+# running_cnts = state.updateStateByKey(update_val)
+# running_cnts.pprint()
+sum_each_minite = state.reduceByKeyAndWindow(update_val, remove_val, window_len)
+sum_each_minite.pprint()
 
 
 ssc.start()
